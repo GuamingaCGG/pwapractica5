2de36677be24dda34086adb1349cec994cdc0fe9
GuamingaCGG <celachristian12@hotmail.com>
2026-05-26 22:01:59 -0500

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-</div>
-<footer class="text-center my-5 text-muted small">
-    <p>&copy; <?php echo date('Y'); ?> - Sistema de Gestión Escolar</p>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
